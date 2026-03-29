13f522f52aeb183a1a04733e14e6ac4cb0ec6764
Mohamedali409 <engmohamedali409@gmail.com>
2026-03-29 09:19:16 +0200

    .

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,21 +1,15 @@
 import axios from "axios";
 
-// 1. إنشاء نسخة من axios مع الرابط الأساسي للـ Backend بتاعك
 const api = axios.create({
   baseURL: "http://localhost:3000/api",
-  // تقدر تضيف هنا headers ثابتة لو حابب
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// 2. عمل Interceptor للـ Requests (عشان نحقن الـ Token لو موجود)
 api.interceptors.request.use(
   (config) => {
-    // هنجيب التوكن من الـ localStorage (المكان اللي هنحفظه فيه بعد الـ Login)
     const token = localStorage.getItem("token");
-
-    // لو التوكن موجود، ضيفه في الـ Header بتاع الطلب
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -27,5 +21,4 @@ api.interceptors.request.use(
   },
 );
 
-// 3. تصدير الـ api عشان نستخدمه في أي مكان في المشروع
 export default api;
